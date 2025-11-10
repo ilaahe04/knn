3f782465460090ec06ae1e95fe83befc2572148e
ilaahe04 <146705323+ilaahe04@users.noreply.github.com>
2025-11-10 22:15:42 +0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+%matplotlib inline
 import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
